f297c9a7ee30a6e42faf3c4108c27c8392cf7209
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-04-27 19:38:39 +0300

    Step 1 done

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import { fileURLToPath } from "node:url";
-import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const fileName = process.argv[2];
-const content = fs.readFileSync(path.join(
-  __dirname,
-  fileName
-), 'utf-8');
+const content = fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
 
 // BEGIN
-console.log(content)
+const rows = content.split('\r\n');
+const data = rows.slice(1);
+
+const creatures = data.map((creature) => creature.split('|')[1]);
+console.log(`Количество видов существ: ${creatures.length}`);
 // END
