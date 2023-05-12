877da876187f31ba136865c9f056fbcfacf193d2
KudesnikRaf <118810105+KudesnikRaf@users.noreply.github.com>
2023-05-12 14:56:00 +0300

    Third step done
    upd step one

--- Makefile
+++ Makefile
@@ -5,7 +5,6 @@ test:
 
 test-steps:
 		npx jest --testNamePattern step1
-		npx jest --testNamePattern step2
 		npx jest --testNamePattern step3
 		npx jest --testNamePattern step4
 		npx jest --testNamePattern step5
--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from "node:url";
 import path from "node:path";
 import fs from 'fs';
 import _ from 'lodash'
+import { workerData } from "node:worker_threads";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,14 +19,30 @@ const content = fs.readFileSync(path.join(
 
 // BEGIN
 //Step 1 
+
 const data = content.split('\n').map((row) => row.split('|').slice(1, -1)).slice(1);
-console.log("Количество рядов:", _.uniq(data.map((row) => (row[0].trim())).sort()).length)
+const names = data.map((row) => row[0]);
+console.log(`Количество рядов: ${names.length}`);
 
-//Step 2
-   data.sort((a, b) => (b[1] - a[1]));
-   console.log(data[0][6] / data[0][3] * 10);
-   console.log(data[1][6] / data[1][3] * 20);
 
-//Step 3
-   
+// Step 2
+const strengthOrder = _.sortBy(data, (creature) =>
+  Number(creature[1])
+);
+const strongestUnit = strengthOrder[strengthOrder.length - 1];
+const secondStrongestUnit = strengthOrder[strengthOrder.length - 2];
+const tenStrongest = strongestUnit[6] * 10;
+const twentySecondStrongest = secondStrongestUnit[6] * 20;
+
+console.log(`цена за 10 сильнейших созданий: ${tenStrongest}
+цена за 20 вторых по силе созданий: ${twentySecondStrongest}`);
+
+
+  //Step 3
+  const someWeightUnit = data.sort((a, b) => b[5] - a[5]);
+  console.log(`цена за отряд самых толстых: ${someWeightUnit[0][3] * someWeightUnit[0][6]}`);
+  console.log(`цена за отряд самых тонких: ${someWeightUnit[someWeightUnit.length - 1][3] * someWeightUnit[someWeightUnit.length - 1][6]}`);
+  
+  //step 4
+  
 // END 
